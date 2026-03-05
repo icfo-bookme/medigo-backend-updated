e733545dab4c5eab38203dc915580a2e01e20707
icfo-bookem <nationalambulancebd@gmail.com>
2026-03-05 10:27:17 +0600

    added

--- Modules/Sale/Resources/views/order-with-call/index.blade.php
+++ Modules/Sale/Resources/views/order-with-call/index.blade.php
@@ -72,7 +72,7 @@
                             </div>
                         </div>
                     </div>
-                    <!--end: Datatable-->
+                  
                 </div>
             </div>
             <!--end::Card-->
